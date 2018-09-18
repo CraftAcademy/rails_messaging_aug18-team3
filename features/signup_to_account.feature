6b478f3567daaeb97745a9f0ff27bfd3a00493a5
Shirmen <shirmenchen@gmail.com>
2018-09-18 16:26:05 +0200

    define spec definition for sign up features

--- features/signup_to_account.feature
+++ features/signup_to_account.feature
@@ -5,13 +5,13 @@ Feature: Signup to an account
 
   Scenario: As a user I would like to signup to an account
     Given I am on the landing page
-    When click on the "Sign up" button
+    When I click on the "Sign up" button
     Then I fill in "Name" with "Bob"
     And I fill in "Email" with "bob@example.com"
     And I fill in "Password" with "foobar"
     And I fill in "Password confirmation" with "foobar"
     When I click "Create"
     Then I should be on the landing page
-    And I should see a message "What message?"
+    
 
 
--- features/step_definitions/signup_definitions_step.rb
+++ features/step_definitions/signup_definitions_step.rb
@@ -1,23 +1,19 @@
 Given("I am on the landing page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path 
 end
 
-When("click on the {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click on the {string} button") do |string|
+  click_on "Sign up"
 end
 
-Then("I fill in {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I fill in {string} with {string}") do |name, user_name|
+  fill_in(name, with: "Bob")
 end
 
 When("I click {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  click_on "Create"
 end
 
 Then("I should be on the landing page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should see a message {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
 end
