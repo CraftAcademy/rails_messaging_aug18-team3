820d592f0a869677ff33277ed377a4aea01c316b
rinbo <robin.b@outlook.com>
2018-09-18 16:06:36 +0200

    Add feature definitions for the signup user story

---
+++ features/step_definitions/signup_definitions_step.rb
@@ -0,0 +1,23 @@
+Given("I am on the landing page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("click on the {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on the landing page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a message {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
